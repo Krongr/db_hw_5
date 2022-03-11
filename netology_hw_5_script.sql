475ff26fac1d9a13cc20261c56a2e64b64a43e00
Artem Nazarov <krongr.study@ya.ru>
2022-03-11 18:30:03 +0300

    Edits after review

--- netology_hw_5_script.sql
+++ netology_hw_5_script.sql
@@ -29,51 +29,40 @@ JOIN track_list tl ON tcp.track_id = tl.id
 JOIN record_album_list ral ON tl.album_id = ral.id
 JOIN singer_album_pair sap ON ral.id = sap.album_id
 JOIN singer_list sl ON sap.singer_id = sl.id
-	WHERE sl.singer_name = 'amazarashi'
-	GROUP BY mcl.collection_name;
+	WHERE sl.singer_name = 'amazarashi';
 
 SELECT ral.album_name
 FROM record_album_list ral
 JOIN singer_album_pair sap ON ral.id =sap.album_id
 JOIN singer_list sl ON sap.singer_id = sl.id
-	WHERE sl.id IN (
-		SELECT singer_id 
-		FROM singer_genre_pair
-			GROUP BY singer_id
-			HAVING COUNT(genre_id) != 1
-	);
+JOIN singer_genre_pair sgp ON sl.id = sgp.singer_id 
+	GROUP BY ral.album_name
+	HAVING COUNT(genre_id) >1;
 
-SELECT track_name
-FROM track_list
-	WHERE id NOT IN (
-		SELECT track_id
-		FROM track_collection_pair
-	);
+SELECT tl.track_name
+FROM track_list tl
+LEFT JOIN track_collection_pair tcp ON tl.id = tcp.track_id 
+	WHERE tcp.track_id IS NULL
 
 SELECT sl.singer_name
 FROM singer_list sl 
 JOIN singer_album_pair sap ON sl.id = sap.singer_id
-	WHERE sap.album_id IN (
-		SELECT album_id 
+JOIN record_album_list ral ON sap.album_id  = ral.id 
+JOIN track_list tl ON ral.id = tl.album_id 
+	WHERE track_length = (
+		SELECT MIN(track_length)
 		FROM track_list
-			WHERE track_length = (
-				SELECT MIN(track_length)
-				FROM track_list
-			)
-	);
+			);
 
-SELECT album_name 
-FROM record_album_list
-	WHERE id IN (
-		SELECT album_id
-		FROM track_list
-			GROUP BY album_id
-			HAVING COUNT(album_id) = (
-				SELECT MIN(count)
-				FROM (
-					SELECT COUNT(album_id)
-					FROM track_list
-						GROUP BY album_id
-				) AS track_count
-			)
+SELECT ral.album_name
+FROM record_album_list ral 
+JOIN track_list tl ON ral.id = tl.album_id 
+	GROUP BY ral.album_name
+	HAVING COUNT(album_id) = (
+		SELECT MIN(count)
+		FROM (
+			SELECT COUNT(album_id)
+			FROM track_list
+				GROUP BY album_id
+		) AS track_count
 	);
